684e24b72e9b2db8c2004b7b590e937e7e465982
Fatma <fatma.janna.utp@gmail.com>
2025-01-26 14:44:43 +0700

    add heart button on header

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import LanguageSwitcher from '../components/LanguageSwitcher';
+import { Heart } from 'lucide-react';
 
 // Import messages directly
 import en from '../../messages/en.json';
@@ -26,8 +27,11 @@ export default async function LocaleLayout({ children, params }) {
     <html lang={locale}>
       <body suppressHydrationWarning={true}>
         <NextIntlClientProvider locale={locale} messages={messages[locale]}>
-          <header className="px-4 py-2">
+          <header className="px-4 py-2 flex justify-between items-center">
             <LanguageSwitcher />
+            <button className="p-2">
+              <Heart className="h-6 w-6" />
+            </button>
           </header>
           {children}
         </NextIntlClientProvider>
