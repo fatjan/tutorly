ee066baa8987ab633b5b0995f3de5ba791e3790b
Fatma <fatma.janna.utp@gmail.com>
2025-01-27 07:02:10 +0700

    refactor dirs and custom layout

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,7 @@ const config = withPWA({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   experimental: {
-    turboMode: true,  // Turbo mode should be enabled here as a boolean flag
-  },
-  turbo: {
-    enabled: true,  // Turbopack-specific options
-    // Additional Turbopack config options can be added here
+    turboMode: true,
   },
 })
 
